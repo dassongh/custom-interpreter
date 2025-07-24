48f68d579c1a2f0f0d072d5f477d4754d63ffd26
Alexander S <oleksandr.slv@gmail.com>
2025-07-24 11:04:43 +0300

    parse if statements

--- ast/ast.go
+++ ast/ast.go
@@ -104,6 +104,19 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+type BlockStatement struct {
+	Token      token.Token // the '{' token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	return out.String()
+}
+
 // Expressions
 type Identifier struct {
 	Token token.Token // the token.IDENT token
@@ -167,3 +180,28 @@ type Boolean struct {
 func (b Boolean) expressionNode()      {}
 func (b Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b Boolean) String() string       { return b.Token.Literal }
+
+type IfExpression struct {
+	Token       token.Token // the 'if' token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
