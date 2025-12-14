6412c36b5018108fa8f7abb78064d9a641cb9064
Alexander S <oleksandr.slv@gmail.com>
2025-12-14 22:24:14 +0200

    improve handling booleans

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/dassongh/custom-interpreter/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -18,7 +23,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -32,4 +37,11 @@ func evalStatements(statements []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
